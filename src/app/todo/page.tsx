1f96554bd2ee32c9de098c9cada8483a2f059099
Mukul Sharma <sharma.mukul@rwit.io>
2024-07-24 11:03:31 +0530

    Added todo page

---
+++ src/app/todo/page.tsx
@@ -0,0 +1,11 @@
+import AddTodo from "../components/AddTodo";
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center min-h-screen py-2">
+      <AddTodo />
+    </div>
+  );
+};
+
+export default page;
